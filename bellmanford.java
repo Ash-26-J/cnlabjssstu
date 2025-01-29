53871e4c6a34f3c04a3a6a230e8a88c191e6b9e9
Ash-26-J <josephashish2012@gmail.com>
2025-01-29 22:36:52 +0530

    Update bellmanford.java

--- bellmanford.java
+++ bellmanford.java
@@ -1,31 +1,44 @@
+//Bellman ford
+
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class CN3 {
-    static String calCRC(String data, String poly, boolean error) {
-        StringBuffer rem = new StringBuffer(data);
-        if (!error)
-            for (int i = 0; i < poly.length() - 1; i++)
-                rem.append("0");
-        for (int i = 0; i < rem.length() - poly.length() + 1; i++)
-            if (rem.charAt(i) == '1')
-                for (int j = 0; j < poly.length(); j++)
-                    rem.setCharAt(i + j, (rem.charAt(i + j) == poly.charAt(j)) ? '0' : '1');
-        return rem.substring(rem.length() - poly.length() + 1);
-    }
+public class CN4 {
+	static int N;
+	static int[][] cost;
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter the data to be sent in bits : ");
-        String data = in.next();
-        String poly = "10000100010001010";
-        String encoded = data + calCRC(data, poly, false);
-        System.out.println("Encoded data : " + encoded);
-        System.out.print("Enter the data that was received : ");
-        String received = in.next();
-        String recString = calCRC(received, poly, true);
-        if (Integer.parseInt(recString, 2) == 0)
-            System.out.println("Data is correct.");
-        else
-            System.out.println("Data is incorrect.");
-    }
+	static void bellmanFord(int src) {
+		int[] dist = new int[N];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[src] = 0;
+		for (int i = 0; i < N; i++)
+			for (int u = 0; u < N; u++)
+				for (int v = 0; v < N; v++)
+					if (dist[u] != Integer.MAX_VALUE && dist[u] + cost[u][v] < dist[v])
+						dist[v] = dist[u] + cost[u][v];
+		for (int u = 0; u < N; u++)
+			for (int v = 0; v < N; v++)
+				if (dist[u] != Integer.MAX_VALUE && dist[u] + cost[u][v] < dist[v]) {
+					System.out.println("Negative weight cycle detected.");
+					return;
+				}
+		System.out.println("Vertex\tDistance from source");
+		for (int i = 0; i < N; i++)
+			System.out.println(i + "\t" + dist[i]);
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the number of vertices : ");
+		N = in.nextInt();
+		cost = new int[N][N];
+		System.out.println("Enter the cost matrix : ");
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				cost[i][j] = in.nextInt();
+		System.out.print("Enter the source vertex : ");
+		int src = in.nextInt();
+		bellmanFord(src);
+
+	}
 }
