edd51f324c65b17eac5a418072fd3ed8a37d53e3
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-03-23 20:02:13 +0530

    Update RSA.java

--- RSA.java
+++ RSA.java
@@ -1,47 +1,40 @@
-import java.util.Scanner;;
+import java.util.*;
 import java.math.BigInteger;
-import java.security.SecureRandom;
-public class RSA{
- private BigInteger p,q,n,phi,e,d;
- private SecureRandom random;
-public RSA(int bitlength){
- random=new SecureRandom();
- p=BigInteger.probablePrime(bitlength,random);
- q=BigInteger.probablePrime(bitlength,random);
- n=p.multiply(q);
- phi=p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
- e=BigInteger.probablePrime(bitlength/2,random);
- while(!phi.gcd(e).equals(BigInteger.ONE)||e.compareTo(phi)>=0)
- {
-  e=BigInteger.probablePrime(bitlength/2,random);
- }
- d=e.modInverse(phi);
+class Rsa
+{
+    BigInteger mod,pub,prv;
+    void getkeys(int bitlen) {
+        Random r = new Random();
+        BigInteger p = BigInteger.probablePrime(bitlen, r);
+        BigInteger q = BigInteger.probablePrime(bitlen, r);
+        mod = p.multiply(q);
+        BigInteger pi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        pub = BigInteger.probablePrime(bitlen / 2, r);
+        prv = pub.modInverse(pi);
+    }
+    BigInteger encrypt(BigInteger m){
+        return m.modPow(pub,mod);
+    }
+    BigInteger decrypt(BigInteger c){
+        return c.modPow(prv,mod);
+    }
 }
-public BigInteger encrypt(BigInteger message){
- return message.modPow(e,n);
+public class Main {
+    public static void main(String[] args) {
+        Rsa r = new Rsa();
+        r.getkeys(1024);
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the message");
+        BigInteger m = new BigInteger(in.next().getBytes());
+        BigInteger c = r.encrypt(m);
+        System.out.println("Encrypted message:" + c.longValue());
+        BigInteger d = r.decrypt(c);
+        System.out.println("decrypt the message:" + new String(d.toByteArray()));
+    }
 }
-public BigInteger decrypt(BigInteger ciphertext){
- return ciphertext.modPow(d,n);
-}
-public BigInteger getPublicKey(){
- return e;
-}
-public BigInteger getMOdulus(){
- return n;
-}
-public static void main(String[] args){
- int bitlength=512;
- RSA rsa=new RSA(bitlength);
- System.out.println("Public key(e,n):( " +rsa.e+" , "+rsa.n+" )");
- System.out.println("Private key(d,n):( " +rsa.d+" , "+rsa.n+" )");
- Scanner in=new Scanner(System.in);
- System.out.println("Enter a message:");
- String plaintext=in.nextLine();
- BigInteger msg=new BigInteger(plaintext.getBytes());
- BigInteger ciphertext=rsa.encrypt(msg);
- System.out.println("Encrypted msg: "+ciphertext);
- BigInteger decrypttext=rsa.decrypt(ciphertext);
- String decrypted=new String(decrypttext.toByteArray());
- System.out.println("Decrypted message:"+decrypted);
-}
-} 
+/*o/p
+ enter the message
+ ashishjoseph
+Encrypted message:-1752810223501621926
+decrypt the message:ashishjoseph
+*/
