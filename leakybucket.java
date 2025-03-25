9d55ac51312a9fe7aa7c7260879496ba8bae28bb
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-03-25 23:25:57 +0530

    Update leakybucket.java

--- leakybucket.java
+++ leakybucket.java
@@ -1,62 +1,38 @@
-import java.util.Scanner;
-public class LeakyBucket{
- public static void main(String args[]){
-  Scanner sc=new Scanner(System.in);
-  System.out.println("Enter number of packets");
-  int n=sc.nextInt();
-  System.out.println("Enter output rate:");
-  int outrate=sc.nextInt();
-  System.out.println("Enter bucket size:");
-  int bsize=sc.nextInt();
-  int [] packets=new int[n];
-  System.out.println("Enter packets sizes in order");
-  for(int i=0;i<n;i++)
-   packets[i]=sc.nextInt();
-  int i=0,cycle=0,remains=0,sent=0,dropped=0;
-  boolean flag=false;
-  System.out.println("Cycle\tPacket\tBucketLimit\tSent\tRemaining");
-  while(true){
-   cycle++;
-   if(packets[i]<=(bsize-remains)){
-    if(remains+packets[i]<=outrate){
-     sent=remains+packets[i];
-     remains=0;
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        int current=0,rem;
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the bucket capacity");
+        int capacity = in.nextInt();
+        System.out.println("enter the rate");
+        int rate =in.nextInt();
+        System.out.println("enter the number of packets");
+        int n = in.nextInt();
+        System.out.println("enter the packet size");
+        int pkt = in.nextInt();
+        rem = capacity - current;
+        if(rem <= pkt){
+            current -= pkt;
+        }else{
+            System.out.println("buffer full");
+            System.out.println(" packet dropped");
+        }
+        System.out.println("current bucket size"+current);
+        current -=rate;
+        System.out.println("after outing"+current);
+        
     }
-    else{
-     remains+=(packets[i]-outrate);
-     if(packets[i]+remains > bsize)
-      dropped=-1;
-     else
-      dropped=packets[i]+remains; 
-     sent=outrate; 
-    }
-    if(!flag)
-     System.out.println(cycle+"\t"+packets[i]+"\t"+dropped+"\t"+sent+"\t"+remains);
-    else
-     System.out.println(cycle+"\t"+packets[i]+"\t------\t"+sent+"\t"+remains);
-   }
-   if(i==(n-1)){
-    flag=true;
-   if(remains==0)
-    break;
-   else{
-    if(remains<=(bsize)){
-     if(remains<=outrate){
-      sent=remains;
-      remains=0;
-     }
-     else{
-      System.out.println(cycle+"\t----\t drop \t 0 \t" +remains);
-      break;
-     }
-   }        
-  }
- } 
-  else
-  {
-  // System.out.println(cycle+"\t"+packets[i]+"\tDrop\t"+sent+"\t"+remains);
-   i++;
-  }
- }
 }
-}     
+/* o/p
+ enter the bucket capacity
+10
+enter the rate
+1
+enter the number of packets
+4
+enter the packet size
+20
+current bucket size-20
+after outing-21
+ */
